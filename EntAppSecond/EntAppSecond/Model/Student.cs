95442230f9db71dc98335be706e6c8f0afc6c455
Hazelmar <46902599+Hazelmar@users.noreply.github.com>
2019-02-20 16:37:27 +0000

    Validation
    Commit

---
+++ EntAppSecond/EntAppSecond/Model/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Student
+{
+    [Required]
+    public string StudentID { get; set; } = " ";
+    [Required]
+    public string FirstName { get; set; } = " ";
+    [Required]
+    public string LastName { get; set; } = " ";
+    public Student()
+	{
+		
+	}
+}
